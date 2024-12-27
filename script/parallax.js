e43a55e9917536cf5710ebc5fff45c3673060589
Anmol Singh <anmolsingh.se@gmail.com>
2024-12-27 14:09:37 +0530

    update

--- script/parallax.js
+++ script/parallax.js
@@ -10,13 +10,52 @@ window.onload = function () {
   lax.addElements(".selector", {
     scrollY: {
       translateY: [
-        [0, 500], // Scroll range
-        [0, 300], // Translation range
-      ],
-      scale: [
-        [0, 500], // Scroll range
-        [1, 1.2], // Scale range
+        ["elCenterY", "elOutY"], // Scroll range
+        ["elCenterY", "screenHeight"], // Translation range
       ],
     },
   });
+  lax.addElements(".slide-left", {
+    scrollY: {
+      translateX: [
+        ["elInY", "elCenterY"], // Scroll range
+        [800, 0], // Translation range
+      ],
+    },
+  });
+  lax.addElements(".slide-right", {
+    scrollY: {
+      translateX: [
+        ["elInY", "elCenterY"], // Scroll range
+        [-800, 0], // Translation range
+      ],
+    },
+  });
+
+  lax.addElements(".page-header", {});
+
+  if (window.innerWidth > 767) {
+    lax.addElements(
+      ".page-header",
+      {
+        scrollY: {
+          translateY: [
+            [0, 500], // Scroll range
+            [0, 30], // Translation range
+          ],
+        },
+      },
+      {
+        onUpdate: function (driverValues, domElement) {
+          const scrollY = driverValues.scrollY[0];
+
+          if (scrollY > 200) {
+            domElement.classList.add("bg-header");
+          } else {
+            domElement.classList.remove("bg-header");
+          }
+        },
+      }
+    );
+  }
 };
